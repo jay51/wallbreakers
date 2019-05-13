a1a48f58a5da279b6ec882755a6a4b8285275d89
jay51 <nenojaafar3@gmail.com>
2019-05-13 03:40:47 -0500

    added xor solution to the find_diff

--- week2/find_diff.py
+++ week2/find_diff.py
@@ -1,4 +1,4 @@
-
+# using ascii math
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
         asc = 0
@@ -9,3 +9,21 @@ class Solution:
             asc -= ord(char) 
             
         return chr(asc)
+
+
+# using xor math
+def findTheDifference(s, t):
+    xor = ord(t[0])
+    for char in t:
+        asc = ord(char) ^ xor
+    
+    for char in s:
+        xor = ord(char) ^ xor 
+        
+    return chr(xor)
+
+s = "abcd"
+t = "abcde"
+print(findTheDifference(s, t))
+
+
