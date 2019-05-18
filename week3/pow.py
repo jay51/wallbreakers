f6e12125b488a0df9c3d99b00d5672b79c562fa3
jay51 <nenojaafar3@gmail.com>
2019-05-18 16:09:50 -0500

    the long solution

--- week3/pow.py
+++ week3/pow.py
@@ -1,6 +1,20 @@
-# This problem is very stupid
 
 class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        print(f"myPow({x}, {n})")
+        if n == 0: return 1;
+        
+        if n < 0:
+            n = -n;
+            x = 1/x;
+            
+        if n % 2 == 0:
+            y = self.myPow(x, n/2)
+            return y * y
+        else:
+            res = x * self.myPow(x, n-1)
+            return res
+            
     def myPow(self, x: float, n: int) -> float:
         
         return x ** n
