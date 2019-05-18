d93a6114c619977009919194df61280b6922f287
jay aljoe <nenojaafar3@gmail.com>
2019-05-18 16:19:34 -0500

    added comments and video explination

--- week3/pow.py
+++ week3/pow.py
@@ -1,13 +1,13 @@
-
+# explination: https://www.youtube.com/watch?v=4ivUTBDnki0
 class Solution:
     def myPow(self, x: float, n: int) -> float:
         print(f"myPow({x}, {n})")
         if n == 0: return 1;
-        
+        # for negative numbers
         if n < 0:
             n = -n;
             x = 1/x;
-            
+        # 2^8 == 2^4 * 2^4
         if n % 2 == 0:
             y = self.myPow(x, n/2)
             return y * y
