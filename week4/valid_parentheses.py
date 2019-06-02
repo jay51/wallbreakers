ba11faee7949532f173d2c8712eef1bfed1f9d76
jay51 <nenojaafar3@gmail.com>
2019-06-02 01:16:57 -0500

    problem: valid parentheses solved with stack

---
+++ week4/valid_parentheses.py
@@ -0,0 +1,31 @@
+"""
+Time : O(n)
+Space: O(1)
+"""
+class Solution:
+    def isValid(self, s: str) -> bool:
+        if not s: return True
+        
+        stack = []
+        for char in s:
+            if char == ")":
+                if not stack or stack.pop() != "(": # check if stack is empty before pop
+                    return False
+                    
+            elif char == "}":
+                if not stack or stack.pop() != "{": # check if stack is empty before pop
+                    return False
+                
+            elif char == "]":
+                if not stack or stack.pop() != "[": # check if stack is empty before pop
+                    return False
+                
+            else:
+                stack.append(char)
+                
+        if stack:
+            return False
+                
+        return True
+    
+
